973d55658c2c8c696c2a097aca59e71ffaad52fb
johnodhiambo0 <johneliud4@gmail.com>
2025-01-24 18:00:25 +0300

    add: dark/light theme functionality for the webpage.

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,25 @@
 'use strict';
+
+const themeToggler = document.querySelector('.theme-toggler');
+
+// Apply theme based on the value in localStorage
+const applyTheme = (theme) => {
+  if (theme === 'dark') {
+    document.body.classList.add('dark-theme');
+  } else {
+    document.body.classList.remove('dark-theme');
+  }
+};
+
+const toggleTheme = () => {
+  const currentTheme = document.body.classList.contains('dark-theme')
+    ? 'dark'
+    : 'light';
+  const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+  applyTheme(newTheme);
+  localStorage.setItem('theme', newTheme);
+};
+const savedTheme = localStorage.getItem('theme') || 'light';
+applyTheme(savedTheme);
+
+themeToggler.addEventListener('click', toggleTheme);
