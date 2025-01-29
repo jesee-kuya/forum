93258480ef640d61ab947a0add534ca3b842bb8f
khahussein <kherld.hussein@gmail.com>
2025-01-29 18:50:28 +0300

    core(main): Implement db/repo and server instances in the main

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "github.com/jesee-kuya/forum/backend/database"
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/jesee-kuya/forum/backend/database"
+	"github.com/jesee-kuya/forum/backend/repositories"
+	"github.com/jesee-kuya/forum/backend/routes"
+)
 
 func main() {
-	database.CreateConnection()
+	db := database.CreateConnection()
+	defer db.Close()
+
+	repositories.InitRepo(db)
+
+	router := routes.RegisterRoutes()
+
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+	}
+
+	log.Println("Server is running on http://localhost:8080")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
