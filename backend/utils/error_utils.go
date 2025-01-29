3777191da9bea565a88cb66e00a447c1205b223e
khahussein <kherld.hussein@gmail.com>
2025-01-29 18:47:03 +0300

    refactor(utils): Rename and replace error utils

---
+++ backend/utils/error_utils.go
@@ -0,0 +1,37 @@
+package utils
+
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+type Message struct {
+	Code       string
+	ErrMessage string
+}
+
+func ErrorHandler(w http.ResponseWriter, errval string, statusCode int) {
+	tmpl, err := template.ParseFiles("frontend/templates/error.html")
+	if err != nil {
+		log.Printf("Failed to load error template: %v", err)
+		http.Error(w, errval, statusCode)
+		return
+	}
+
+	code := strconv.Itoa(statusCode)
+
+	data := Message{
+		Code:       code,
+		ErrMessage: errval,
+	}
+
+	w.WriteHeader(statusCode)
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Error executing the template: %v", err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+}
