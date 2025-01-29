44768e6a9fa880bb51ef784a799456c230fe3b26
jamos <amosjoel91@gmail.com>
2025-01-29 19:13:59 +0300

    fix: remove conflicting template data feat: add signup handler refactor: redefine the methods for better result

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -8,9 +8,7 @@ import (
 	"github.com/jesee-kuya/forum/backend/models"
 )
 
-type Response struct {
-	pageTitle string
-}
+var user models.User
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	// URL path
@@ -34,10 +32,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	data := Response{
-		pageTitle: "Home",
-	}
-	tmpl.Execute(w, data)
+	tmpl.ExecuteTemplate(w, "index,html", nil)
 }
 
 func LoginHandler(w http.ResponseWriter, r *http.Request) {
@@ -47,26 +42,55 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if r.Method == http.MethodPost {
+		if user.Username == "" || user.Email == "" || user.Password == "" {
+			ErrorHandler(w, "Username/Email and Password are required", http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
+		return
+
+	}
+	if r.Method == http.MethodGet {
+		tmpl, err := template.ParseFiles("frontend/templates/sign-in.html")
+		if err != nil {
+			ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
+		}
+
+		tmpl.ExecuteTemplate(w, "sign-in.html", nil)
+	}
+	ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+}
+
+func SignupHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/sign-up" {
+		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == http.MethodPost {
 		fmt.Println("OK: ", http.StatusOK)
-	} else {
-		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		r.ParseForm()
+		user.Username = r.PostFormValue("username")
+		user.Email = r.PostFormValue("email")
+		user.Password = r.PostFormValue("password")
+
+		if user.Username == "" || user.Email == "" || user.Password == "" {
+			ErrorHandler(w, "Fields cannot be empty", http.StatusBadRequest)
+			return
+		}
+
+		http.Redirect(w, r, "/sign-in", http.StatusSeeOther)
 		return
 	}
 
-	var user models.User
-	if user.Username == "" || user.Email == "" || user.Password == "" {
-		ErrorHandler(w, "Username/Email and Password are required", http.StatusBadRequest)
-		return
-	}
+	if r.Method == http.MethodGet {
+		tmpl, err := template.ParseFiles("frontend/templates/sign-up.html")
+		if err != nil {
+			ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
+		}
 
-	tmpl, err := template.ParseFiles("frontend/templates/sign-in.html")
-	if err != nil {
-		ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
+		tmpl.ExecuteTemplate(w, "sign-up.html", nil)
 	}
-
-	data := Response{
-		pageTitle: "sign-in",
-	}
-
-	tmpl.Execute(w, data)
+	ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 }
