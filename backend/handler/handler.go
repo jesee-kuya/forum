21055f94e4b1e943d3f11dfe5254938b77c83133
jamos <amosjoel91@gmail.com>
2025-01-29 16:55:10 +0300

    feat: add sign in handler

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"text/template"
+
+	"github.com/jesee-kuya/forum/backend/models"
 )
 
 type Response struct {
@@ -17,12 +19,15 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// Method
+	// Method used
 	if r.Method == http.MethodGet {
 		fmt.Println("OK: ", http.StatusOK)
+	} else {
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
 
-	// template
+	// template rendering
 	tmpl, err := template.ParseFiles("frontend/templates/index.html")
 	if err != nil {
 		ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
@@ -35,3 +40,33 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, data)
 }
 
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/sign-in" {
+		ErrorHandler(w, "Page not found", http.StatusNotFound)
+		return
+	}
+
+	if r.Method == http.MethodPost {
+		fmt.Println("OK: ", http.StatusOK)
+	} else {
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var user models.User
+	if user.Username == "" || user.Email == "" || user.Password == "" {
+		ErrorHandler(w, "Username/Email and Password are required", http.StatusBadRequest)
+		return
+	}
+
+	tmpl, err := template.ParseFiles("frontend/templates/sign-in.html")
+	if err != nil {
+		ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
+	}
+
+	data := Response{
+		pageTitle: "sign-in",
+	}
+
+	tmpl.Execute(w, data)
+}
