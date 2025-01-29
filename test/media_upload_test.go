fdc1548dc36b2ccecccaff4a738a96b8b2424d77
jkuya <kuyajesee@proton.me>
2025-01-29 13:56:51 +0300

    Refactor: refactor the code to test the body returned

--- test/media_upload_test.go
+++ test/media_upload_test.go
@@ -3,16 +3,39 @@ package test
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
+
+	"github.com/jesee-kuya/forum/backend/handler"
 )
 
-func TestUploadMedia(t *testing.T) {
-	req := httptest.NewRequest("GET", "http://localhost:8080", nil)
-	w := httptest.NewRecorder()
-	UploadMedia(w, req)
-	resp := w.Result()
+var TestCase2 = []struct {
+	name         string
+	method       string
+	endPoint     string
+	expectedBody string
+	code         int
+}{
+	{"Test1", http.MethodGet, "/upload", "Invalid reques method", http.StatusMethodNotAllowed},
+}
 
-	if resp.StatusCode != http.StatusOK {
-		t.Errorf("Expected the statusCode %v but got the statuscode %v", http.StatusOK, resp.StatusCode)
+func TestUploadMedia(t *testing.T) {
+	for _, tc := range TestCase2 {
+		t.Run(tc.name, func(t *testing.T) {
+			req := httptest.NewRequest(tc.method, tc.endPoint, nil)
+			w := httptest.NewRecorder()
+
+			handler.UploadMedia(w, req)
+
+			resp := w.Result()
+			if resp.StatusCode != tc.code {
+				t.Errorf("Expected status %d, got %d", http.StatusMethodNotAllowed, resp.StatusCode)
+			}
+
+			expectedBody := "Invalid request method"
+			if !strings.Contains(w.Body.String(), expectedBody) {
+				t.Errorf("Expected response body to contain %q, got %q", expectedBody, w.Body.String())
+			}
+		})
 	}
 }
