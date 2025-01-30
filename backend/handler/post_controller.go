8f47e03c22f26e09deb9bfafc3fe630b66967f36
jamos <amosjoel91@gmail.com>
2025-01-30 19:01:29 +0300

    feat: serve post to template from the database

--- backend/handler/post_controller.go
+++ backend/handler/post_controller.go
@@ -1,27 +1,31 @@
 package handler
 
 import (
+	"database/sql"
 	"encoding/json"
 	"log"
 	"net/http"
+	"text/template"
 
 	"github.com/jesee-kuya/forum/backend/database"
 	"github.com/jesee-kuya/forum/backend/repositories"
 )
 
-func GetAllPosts(w http.ResponseWriter, r *http.Request) {
-	db := database.CreateConnection()
-	posts, err := repositories.GetPosts(db)
-	if err != nil {
-		log.Printf("Failed to get posts: %v", err)
-		ErrorHandler(w, "Internal Server Error", http.StatusMethodNotAllowed)
-		return
-	}
+func GetAllPosts(db *sql.DB, tmpl *template.Template) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		db := database.CreateConnection()
+		posts, err := repositories.GetPosts(db)
+		if err != nil {
+			log.Printf("Failed to get posts: %v", err)
+			ErrorHandler(w, "Internal Server Error", http.StatusMethodNotAllowed)
+			return
+		}
 
-	err = json.NewEncoder(w).Encode(posts)
-	if err != nil {
-		log.Printf("Failed to encode posts to JSON: %v", err)
-		ErrorHandler(w, "Internal Server Error", http.StatusMethodNotAllowed)
-		return
+		err = json.NewEncoder(w).Encode(posts)
+		if err != nil {
+			log.Printf("Failed to encode posts to JSON: %v", err)
+			ErrorHandler(w, "Internal Server Error", http.StatusMethodNotAllowed)
+			return
+		}
 	}
 }
