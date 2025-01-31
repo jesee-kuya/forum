f1ffa348a1fdc7d8b05fd7c7adbfec54b0a9a558
jamos <amosjoel91@gmail.com>
2025-01-31 11:57:43 +0300

    feat: add Comments in struct for nested comments fix: change render type to json for conflicting route fix: fix error in comment fetching

--- backend/handler/post_controller.go
+++ backend/handler/post_controller.go
@@ -5,13 +5,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"text/template"
 
-	"github.com/jesee-kuya/forum/backend/models"
 	"github.com/jesee-kuya/forum/backend/repositories"
 )
 
-func GetAllPosts(db *sql.DB, tmpl *template.Template) http.HandlerFunc {
+func GetAllPosts(db *sql.DB) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		posts, err := repositories.GetPosts(db)
 		if err != nil {
@@ -19,13 +17,21 @@ func GetAllPosts(db *sql.DB, tmpl *template.Template) http.HandlerFunc {
 			ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
 			return
 		}
+		// fetch comments for each post
+		for i, post := range posts {
+			comments, err := repositories.GetComments(db, post.ID)
+			if err != nil {
+				log.Printf("Failed to get posts: %v", err)
+				ErrorHandler(w, "Internal Server Error", http.StatusInternalServerError)
+				return
+			}
 
-		// Set content type to html and render the template
-		w.Header().Set("Content-Type", "text/html")
+			posts[i].Comments = comments
+		}
 
-		err = tmpl.ExecuteTemplate(w, "index.html", struct {
-			Posts []models.Post
-		}{Posts: posts})
+		// Set content type to application/json and serve API endpoint
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
 
 		if err = json.NewEncoder(w).Encode(posts); err != nil {
 			log.Printf("Failed to encode posts to JSON: %v", err)
--- backend/models/models.go
+++ backend/models/models.go
@@ -22,7 +22,7 @@ type Post struct {
 	UserName     string    `json:"username"`
 	PostTitle    string    `json:"post_title"`
 	Body         string    `json:"body"`
-	ParentID     int       `json:"parent_id"`
+	ParentID     *int       `json:"parent_id"`
 	CreatedOn    time.Time `json:"created_on"`
 	PostCategory string    `json:"post_category"`
 	PostStatus   string    `json:"post_status"`
@@ -30,6 +30,7 @@ type Post struct {
 	Dislikes     int       `json:"dislikes"`
 	CommentCount int       `json:"comment_count"`
 	ImageURL     string    `json:"imageurl"`
+	Comments     []Post
 }
 
 // File model
