20bae0a8e25583d8689b402641e23451e9a7c91a
jamos <amosjoel91@gmail.com>
2025-02-04 11:54:55 +0300

    feat: add a password encryption logic

---
+++ backend/util/password_encrypt.go
@@ -0,0 +1,30 @@
+package util
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/bcrypt"
+)
+
+func PasswordEncrypt(password []byte, cost int) ([]byte, error) {
+	if len(password) < 8 || len(password) > 64 {
+		return nil, errors.New("password length must be between 8 and 64 characters")
+	}
+	// Password encryption logic
+	hashedPassword, err := bcrypt.GenerateFromPassword(password, cost)
+	if err != nil {
+		return nil, err
+	}
+
+	return hashedPassword, nil
+}
+
+func CompareHashAndPassword(hashedPassword, password []byte) error {
+	// Compare hashed password with the password provided
+	err := bcrypt.CompareHashAndPassword(hashedPassword, password)
+	if err != nil {
+		return errors.New("invalid password")
+	}
+
+	return nil
+}
