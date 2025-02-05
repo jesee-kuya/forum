5e83c2708530362962b914c25e9ea71f992e59d7
johnodhiambo0 <johneliud4@gmail.com>
2025-02-05 14:58:28 +0300

    fix: remove embedded middleware to routes handler.

--- backend/route/routes.go
+++ backend/route/routes.go
@@ -4,30 +4,26 @@ import (
 	"net/http"
 
 	"github.com/jesee-kuya/forum/backend/handler"
-	"github.com/jesee-kuya/forum/backend/middleware"
 	"github.com/jesee-kuya/forum/backend/util"
 )
 
 func InitRoutes() *http.ServeMux {
 	r := http.NewServeMux()
 
-	// Serve static files (CSS, JS, images)
 	fs := http.FileServer(http.Dir("./frontend"))
 	r.Handle("/frontend/", http.StripPrefix("/frontend/", fs))
 
-	// Serve uploaded media files
 	uploadFs := http.FileServer(http.Dir("./uploads"))
 	r.Handle("/uploads/", http.StripPrefix("/uploads/", uploadFs))
 
 	// App routes
-	r.HandleFunc("/", handler.IndexHandler)
+	r.HandleFunc("/home", handler.IndexHandler)
+	r.HandleFunc("/", handler.HomeHandler)
 	r.HandleFunc("/sign-in", handler.LoginHandler)
 	r.HandleFunc("/sign-up", handler.SignupHandler)
-	r.HandleFunc("/upload", middleware.Authenticate(handler.CreatePost))
-	r.HandleFunc("/logout", middleware.Authenticate(handler.Logout))
+	r.HandleFunc("/upload", handler.CreatePost)
+	r.HandleFunc("/logout", handler.Logout)
 
-	// r.HandleFunc("/posts", handler.GetAllPosts(db, tmpl))
 	r.HandleFunc("/api/posts", handler.GetAllPostsAPI(util.DB))
-
 	return r
 }
