31e1b52d40b78454460726e8c97c0a2bc2d32cc9
jkuya <kuyajesee@proton.me>
2025-02-05 17:54:40 +0300

    Feat: write querys to execute and handle reactions

--- backend/repositories/reaction_repository.go
+++ backend/repositories/reaction_repository.go
@@ -3,6 +3,7 @@ package repositories
 import (
 	"database/sql"
 	"fmt"
+	"log"
 
 	"github.com/jesee-kuya/forum/backend/models"
 )
@@ -37,4 +38,63 @@ func GetReactions(db *sql.DB, id int, react string) ([]models.Reaction, error) {
 	}
 
 	return Reactions, nil
+}
+
+func CheckReactions(db *sql.DB, userId, postId int) (bool, string) {
+	query := `
+    SELECT * FROM tblReactions
+    WHERE post_id = ? AND user_id = ?
+`
+
+	rows, err := db.Query(query, postId, userId)
+	if err != nil {
+		log.Println("error executing query:", err)
+		return false, ""
+	}
+	defer rows.Close()
+
+	if !rows.Next() {
+		log.Println("No matching rows found")
+		return false, ""
+	}
+
+	reaction := models.Reaction{}
+
+	err = rows.Scan(&reaction.ID, &reaction.Reaction, &reaction.ReactionStatus, &reaction.UserID, &reaction.PostID)
+	if err != nil {
+		return false, ""
+	}
+
+	return true, reaction.Reaction
+}
+
+func UpdateReaction(db *sql.DB, reaction string, userId, postId int) {
+	query := `
+	UPDATE tblReactions
+	SET reaction = ?
+	 WHERE post_id = ? AND user_id = ?
+	`
+	_, err := db.Query(query, reaction, postId, userId)
+	if err != nil {
+		log.Println("error executing query:", err)
+		return
+	}
+}
+
+func UpdateReactionStatus(db *sql.DB, userId, postId int) {
+	query := `
+    UPDATE tblReactions
+    SET reaction_status = 
+        CASE 
+            WHEN reaction_status = 'clicked' THEN 'unclicked'
+            ELSE 'clicked'
+        END
+    WHERE post_id = ? AND user_id = ?
+`
+
+	_, err := db.Exec(query, postId, userId) // Use Exec instead of Query for UPDATE
+	if err != nil {
+		log.Println("Error executing query:", err)
+		return
+	}
 }
