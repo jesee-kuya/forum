ed203ac489f907aee4c42ea171a4d9e8ec4c05e5
khahussein <kherld.hussein@gmail.com>
2025-02-06 14:07:37 +0300

    refactor(handler): Update logout handler with custom getSession func for better readability

--- backend/handler/handler.go
+++ backend/handler/handler.go
@@ -312,36 +312,25 @@ func LoginHandler(w http.ResponseWriter, r *http.Request) {
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		util.ErrorHandler(w, "No active session", http.StatusUnauthorized)
+		util.ErrorHandler(w, "Invalid request method", http.StatusMethodNotAllowed)
 		return
 	}
 
-	cookie, err := r.Cookie("session_token")
+	session, err := getSession(r)
 	if err != nil {
 		util.ErrorHandler(w, "No active session", http.StatusUnauthorized)
 		return
 	}
 
-	err = repositories.DeleteSession(cookie.Value)
-	if err != nil {
-		util.ErrorHandler(w, "Failed to log out", http.StatusInternalServerError)
-		return
-	}
-
-	for i, session := range Sessions {
-		if session.Token == cookie.Value {
+	repositories.DeleteSession(session.Token)
+	for i, s := range Sessions {
+		if s.Token == session.Token {
 			Sessions[i] = StoreSession{}
 			break
 		}
 	}
 
-	http.SetCookie(w, &http.Cookie{
-		Name:     "session_token",
-		Value:    "",
-		Path:     "/",
-		Expires:  time.Now().Add(-time.Hour),
-		HttpOnly: true,
-	})
+	http.SetCookie(w, &http.Cookie{Name: "session_token", Value: "", Path: "/", Expires: time.Now().Add(-time.Hour), HttpOnly: true})
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
