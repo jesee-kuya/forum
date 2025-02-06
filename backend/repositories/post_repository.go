644061fe2ce0d582c29ebef9c0007a8ee2844bea
joseowino <joeoowino@gmail.com>
2025-02-06 14:58:34 +0300

    refactor: Update filter posts handler

--- backend/repositories/post_repository.go
+++ backend/repositories/post_repository.go
@@ -54,14 +54,10 @@ func GetComments(db *sql.DB, id int) ([]models.Post, error) {
 }
 
 func FilterPostsByCategories(db *sql.DB, categories []string) ([]models.Post, error) {
-	if len(categories) == 0 {
-		return nil, fmt.Errorf("no categories provided")
-	}
-
 	placeholders := strings.Repeat("?,", len(categories)-1) + "?"
 
 	query := fmt.Sprintf(`
-		SELECT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
+		SELECT DISTINCT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
 		FROM tblPosts p
 		JOIN tblUsers u ON p.user_id = u.id
 		LEFT JOIN tblPostCategories c ON p.id = c.post_id 
@@ -88,42 +84,43 @@ func FilterPostsByCategories(db *sql.DB, categories []string) ([]models.Post, er
 	return posts, nil
 }
 
-// FilterPosts - Fetch posts based on category or user
-func FilterPosts(db *sql.DB, filterType, filterValue string) ([]models.Post, error) {
-	var query string
-	var rows *sql.Rows
-	var err error
-
-	switch filterType {
-	case "category":
-		query = `
-			SELECT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
-			FROM tblPosts p
-			JOIN tblUsers u ON p.user_id = u.id
-			LEFT JOIN tblPostCategories c ON p.id = c.post_id 
-			WHERE p.parent_id IS NULL AND p.post_status = 'visible' 
-			AND c.category = ? 
-			`
-	case "user":
-		query = `
-		SELECT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
+func FilterPostsByUser(db *sql.DB, id int) ([]models.Post, error) {
+	query := `
+		SELECT DISTINCT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
 		FROM tblPosts p
 		JOIN tblUsers u ON p.user_id = u.id
 		WHERE p.parent_id IS NULL AND p.post_status = 'visible' AND u.id = ?
 		`
-	case "likes":
-		query = `
-		SELECT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
+
+	rows, err := db.Query(query, id)
+	if err != nil {
+		return nil, fmt.Errorf("failed to execute query: %v", err)
+	}
+	defer rows.Close()
+
+	posts, err := processSQLData(rows)
+	if err != nil {
+		return nil, fmt.Errorf("failed process posts: %v", err)
+	}
+
+	return posts, err
+}
+
+// FilterPosts - Fetch posts based on category or user
+func FilterPostsByLikes(db *sql.DB, id int) ([]models.Post, error) {
+	query := `
+		SELECT DISTINCT p.id, p.user_id, u.username, p.post_title, p.body, p.created_on, p.media_url
 		FROM tblPosts p
 		JOIN tblUsers u ON p.user_id = u.id
 		LEFT JOIN tblReactions r ON p.id = r.post_id 
-		WHERE p.parent_id IS NULL AND p.post_status = 'visible' AND r.reaction_status = 'clicked' AND r.reaction = 'Like' AND r.user_id = ?
+		WHERE p.parent_id IS NULL 
+		AND p.post_status = 'visible' 
+		AND r.reaction_status = 'clicked' 
+		AND r.reaction = 'Like' 
+		AND r.user_id = ?
 		`
-	default:
-		return nil, fmt.Errorf("invalid filter type")
-	}
 
-	rows, err = db.Query(query, filterValue)
+	rows, err := db.Query(query, id)
 	if err != nil {
 		return nil, fmt.Errorf("failed to execute query: %v", err)
 	}
