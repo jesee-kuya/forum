920939fa756c9d1afbc6e0643fc6401bbda264b3
khahussein <kherld.hussein@gmail.com>
2025-02-10 13:06:25 +0300

    core(route): Enpapsulate the routes with session middleware

--- backend/route/routes.go
+++ backend/route/routes.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/jesee-kuya/forum/backend/handler"
+	"github.com/jesee-kuya/forum/backend/middleware"
 	"github.com/jesee-kuya/forum/backend/util"
 )
 
@@ -21,13 +22,13 @@ func InitRoutes() *http.ServeMux {
 	r.HandleFunc("/", handler.HomeHandler)
 	r.HandleFunc("/sign-in", handler.LoginHandler)
 	r.HandleFunc("/sign-up", handler.SignupHandler)
-	r.HandleFunc("/upload", handler.CreatePost)
-	r.HandleFunc("/logout", handler.LogoutHandler)
-	r.HandleFunc("/comments", handler.CommentHandler)
-	r.HandleFunc("/reaction", handler.ReactionHandler)
-	r.HandleFunc("/likes", handler.ReactionHandler)
-	r.HandleFunc("/dilikes", handler.ReactionHandler)
-	r.HandleFunc("/filter", handler.FilterPosts)
+	r.Handle("/upload", middleware.SessionMiddleware(http.HandlerFunc(handler.CreatePost)))
+	r.Handle("/logout", middleware.SessionMiddleware(http.HandlerFunc(handler.LogoutHandler)))
+	r.Handle("/comments", middleware.SessionMiddleware(http.HandlerFunc(handler.CommentHandler)))
+	r.Handle("/reaction", middleware.SessionMiddleware(http.HandlerFunc(handler.ReactionHandler)))
+	r.Handle("/likes", middleware.SessionMiddleware(http.HandlerFunc(handler.ReactionHandler)))
+	r.Handle("/dilikes", middleware.SessionMiddleware(http.HandlerFunc(handler.ReactionHandler)))
+	r.Handle("/filter", middleware.SessionMiddleware(http.HandlerFunc(handler.FilterPosts)))
 
 	r.HandleFunc("/api/posts", handler.GetAllPostsAPI(util.DB))
 	r.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
