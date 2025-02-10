032f6bd6072d7e8bd563b0bbef327ca64d4da687
jkuya <kuyajesee@proton.me>
2025-02-10 18:17:42 +0300

    refactor: log errors

--- backend/handler/logouthandler.go
+++ backend/handler/logouthandler.go
@@ -10,19 +10,21 @@ import (
 
 func LogoutHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		util.ErrorHandler(w, "No active session", http.StatusUnauthorized)
+		log.Println("method not allowed")
+		util.ErrorHandler(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
 	cookie, err := getSessionID(r)
 	if err != nil {
-		log.Println("Invalid Session")
+		log.Println("Invalid Session:", err)
 		http.Redirect(w, r, "/", http.StatusSeeOther)
 		return
 	}
 
 	err = repositories.DeleteSession(cookie)
 	if err != nil {
+		log.Println("error deleting session:", err)
 		util.ErrorHandler(w, "An Unexpected Error Occurred. Try Again Later", http.StatusInternalServerError)
 		return
 	}
