1ae59f758eefc627fff46d9aa6304caa0c31520a
jamos <amosjoel91@gmail.com>
2025-02-11 11:33:02 +0300

    feat: restrict sending empty comments

--- backend/handler/comments.go
+++ backend/handler/comments.go
@@ -35,6 +35,11 @@ func CommentHandler(w http.ResponseWriter, r *http.Request) {
 	id := r.FormValue("id")
 	userId := sessionData["userId"].(int)
 	comment := r.FormValue("comment")
+	if comment == "" {
+		log.Println("Empty comment")
+		http.Redirect(w, r, "/home", http.StatusSeeOther)
+		return
+	}
 
 	repositories.InsertRecord(util.DB, "tblPosts", []string{"user_id", "body", "parent_id", "post_title"}, userId, comment, id, "comment")
 	http.Redirect(w, r, "/home", http.StatusSeeOther)
