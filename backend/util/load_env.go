2041a1427186b60db8f823f03c53f725d415d057
John <johneliud4@gmail.com>
2025-02-22 11:19:59 +0300

    fix: get environment variable values directly after setting them since os.Setenv()may not be available in fetching them immediately.

--- backend/util/load_env.go
+++ backend/util/load_env.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+var (
+	GoogleClientID, GoogleClientSecret, GithubClientID, GithubClientSecret string
+)
+
 // LoadEnv reads the .env file to set envrionment variables.
 func LoadEnv(filename string) error {
 	file, err := os.Open(filename)
@@ -27,12 +31,16 @@ func LoadEnv(filename string) error {
 		if len(parts) != 2 {
 			continue
 		}
-
 		key := strings.TrimSpace(parts[0])
-		value := strings.Trim(strings.TrimSpace(parts[1]), `"`)
+		value := strings.TrimSpace(parts[1])
 
 		// Set environment variable
 		os.Setenv(key, value)
 	}
+	GoogleClientID = os.Getenv("GOOGLE_CLIENT_ID")
+	GoogleClientSecret = os.Getenv("GOOGLE_CLIENT_SECRET")
+	GithubClientID = os.Getenv("GITHUB_CLIENT_ID")
+	GithubClientSecret = os.Getenv("GITHUB_CLIENT_SECRET")
+
 	return scanner.Err()
 }
