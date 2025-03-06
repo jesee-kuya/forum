24dd8742fabe99430e4c13f01d309f5be6c304a8
jkuya <kuyajesee@proton.me>
2025-03-06 18:33:02 +0300

    feat: add TLS support with Let's Encrypt using autocert for secure connections

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"crypto/tls"
 	"log"
 	"net/http"
 	"time"
 
 	"github.com/jesee-kuya/forum/backend/route"
 	"github.com/jesee-kuya/forum/backend/util"
+	"golang.org/x/crypto/acme/autocert"
 )
 
 func main() {
@@ -20,12 +22,23 @@ func main() {
 	}
 	router := route.InitRoutes()
 
+	certManager := autocert.Manager{
+		Prompt:     autocert.AcceptTOS,
+		HostPolicy: autocert.HostWhitelist("localhost"),
+		Cache:      autocert.DirCache("certs"),
+	}
+
 	server := &http.Server{
 		Addr:         port,
 		Handler:      router,
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
+		TLSConfig: &tls.Config{
+			GetCertificate: certManager.GetCertificate,
+		},
 	}
+
+	go http.ListenAndServe(":80", certManager.HTTPHandler(nil))
 
 	log.Printf("Server started at http://localhost%s\n", port)
 	if err = server.ListenAndServe(); err != nil {
